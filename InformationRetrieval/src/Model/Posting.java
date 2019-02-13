620fd29831e87daa4c609eda10ef318209de7362
admin <admin@15a>
2019-02-13 12:49:20 +0700

    Commit latihan ke3

---
+++ InformationRetrieval/src/Model/Posting.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author admin
+ */
+public class Posting {
+    private Document document;
+
+    public Posting(Document document) {
+        this.document = document;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+    
+    
+}
